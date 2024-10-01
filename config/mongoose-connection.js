8cb89597ac8e80706aa5a97875c7db33023bd2e4
Shaharyar Ahmed <shaharyar.ahmed000@gmail.com>
2024-10-01 17:40:46 +0500

    development/production test + owner creation

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,12 +1,28 @@
 const mongoose = require("mongoose");
+const config = require("config");
+const dbgr = require("debug")("development:mongoose");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/scatch")
+  .connect(`${config.get("MONGODB_URI")}/scatch`)
   .then(function () {
-    console.log("connected");
+    dbgr("Connected to MongoDB successfully.");
   })
   .catch(function (err) {
-    console.log(err);
+    dbgr("Error connecting to MongoDB:", err);
   });
 
 module.exports = mongoose.connection;
+
+// const mongoose = require("mongoose");
+// const dbgr = require("debug")("development:mongoose");
+
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/scatch")
+//   .then(function () {
+//     dbgr("connected");
+//   })
+//   .catch(function (err) {
+//     dbgr(err);
+//   });
+
+// module.exports = mongoose.connection;
